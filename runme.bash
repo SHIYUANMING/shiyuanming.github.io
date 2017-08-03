8f0433c0ddc8b7be9be99bd8e5e39c21fbd95d17
SHIYUANMING <SHIYUANMING@users.noreply.github.com>
2017-08-03 18:36:24 +0800

    Add files via upload

--- runme.bash
+++ runme.bash
@@ -3,3 +3,6 @@ python jemdoc.py education.jemdoc
 python jemdoc.py research.jemdoc
 python jemdoc.py publications.jemdoc
 python jemdoc.py teaching.jemdoc
+python jemdoc.py group.jemdoc
+python jemdoc.py talks.jemdoc
+python jemdoc.py index.jemdoc
